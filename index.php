c3bbad5e49582ecc88fc6ab3ec8a6ce2f1aa663f
murum <cr222cs@student.lnu.se>
2013-03-01 10:57:24 +0100

    strängberoenden väck

--- index.php
+++ index.php
@@ -1,36 +1,68 @@
 <?php
-	include_once("ChartLyricsWebService.php");
-	$client = new ChartLyricsService();
-	try
-	{
-		if(isset($_GET['lyricChecksum']) && isset($_GET['lyricId']))
+
+	require_once("ChartLyricsWebService.php");
+
+	class SoapExperiment {
+	
+		private $client;
+		
+		private $artist = "Michael Jackson";
+		private $song = "Bad";
+		
+		CONST GET_CHECKSUM = "lyricChecksum";
+		CONST GET_ID = "lyricId";
+		
+		CONST LYRIC = 'lyric';
+		CONST LYRIC_CHECKSUM = 'lyricChecksum';
+		CONST LYRIC_ID = 'lyricId';
+		CONST ARTIST = 'artist';
+		CONST SONG = 'song';
+		
+		CONST ERROR = 'error';
+		
+		public function __construct()
 		{
-			$getLyric = new GetLyric();
-			$getLyric->lyricCheckSum = $_GET['lyricChecksum'];
-			$getLyric->lyricId = (int)$_GET['lyricId'];
-			$getLyricResponse = $client->GetLyric($getLyric);
-			$lyricResult = $getLyricResponse->GetLyricResult;
-			$lyrics = array('lyric' => $lyricResult->Lyric, 'artist' => $lyricResult->LyricArtist, 'song' => $lyricResult->LyricSong);
+			$this->client = new ChartLyricsService();
 		}
-		else
+		
+		public function DoExperiment()
 		{
-			$searchLyric = new SearchLyric();
-			$searchLyric->artist = "Michael Jackson";
-			$searchLyric->song = "Bad";
-			
-			$searchLyricResponse = new SearchLyricResponse();
-			
-			$searchLyricResponse = $client->SearchLyric($searchLyric);
-			
-			foreach($searchLyricResponse->SearchLyricResult->SearchLyricResult as $searchLyricResult)
+			try
 			{
-				$result[]= array('artist' => $searchLyricResult->Artist, 'song' => $searchLyricResult->Song, 'lyricChecksum' => $searchLyricResult->LyricChecksum, 'lyricId' => $searchLyricResult->LyricId);
+				if(isset($_GET[self::GET_CHECKSUM]) && isset($_GET[self::GET_ID]))
+				{
+					$getLyric = new GetLyric();
+					$getLyric->lyricCheckSum = $_GET[self::GET_CHECKSUM];
+					$getLyric->lyricId = (int)$_GET[self::GET_ID];
+					$getLyricResponse = $this->client->GetLyric($getLyric);
+					$lyricResult = $getLyricResponse->GetLyricResult;
+					return array(self::LYRIC => $lyricResult->Lyric, self::ARTIST => $lyricResult->LyricArtist, self::SONG => $lyricResult->LyricSong);
+				}
+				else
+				{
+					$searchLyric = new SearchLyric();
+					$searchLyric->artist = $this->artist;
+					$searchLyric->song = $this->song;
+					
+					$searchLyricResponse = new SearchLyricResponse();
+					
+					$searchLyricResponse = $this->client->SearchLyric($searchLyric);
+					
+					foreach($searchLyricResponse->SearchLyricResult->SearchLyricResult as $searchLyricResult)
+					{
+						$result[] = array(self::ARTIST => $searchLyricResult->Artist, self::SONG => $searchLyricResult->Song, self::LYRIC_CHECKSUM => $searchLyricResult->LyricChecksum, self::LYRIC_ID => $searchLyricResult->LyricId);
+					}
+					return $result;
+				}
+			} catch (Exception $e)
+			{
+				return array(self::ERROR => "Vänligen, vänta några sekunder och testa igen!");
 			}
 		}
-	} catch (Exception $e)
-	{
-		$error = "Vänligen, vänta några sekunder och testa igen!";
 	}
+
+	$se = new SoapExperiment();
+	$body = $se->DoExperiment();
 ?>
 
 <!DOCTYPE html>
@@ -41,29 +73,39 @@
 </head>
 <body>
 	<div id="container">
-		<h1>Grym hemsida</h1>
-		<ul>
+		<h1>Grym hemsida</h1>		
 		<?php
-			if(isset($result))
+			if(isset($body[0][SoapExperiment::LYRIC_CHECKSUM]))
 			{
-				foreach ($result as $key) {
-					if(isset($key['lyricChecksum']) && isset($key['lyricId']))
-					echo "<li><a href='?lyricChecksum=".$key['lyricChecksum']."&lyricId=".$key['lyricId']."'>".$key['artist']." - ".$key['song']."</a></li>";
+				?>
+				<ul>
+				<?php
+				foreach ($body as $key) {
+					if(isset($key[SoapExperiment::LYRIC_CHECKSUM]) && isset($key[SoapExperiment::LYRIC_ID]))
+					echo "<li>
+							<a 
+								href='?".SoapExperiment::GET_CHECKSUM."=".$key[SoapExperiment::LYRIC_CHECKSUM]."&".SoapExperiment::GET_ID."=".$key[SoapExperiment::LYRIC_ID]."'
+							>"
+								.$key[SoapExperiment::ARTIST]." - ".$key[SoapExperiment::SONG]."
+							 </a>
+						 </li>";
 				}
+				?>
+				</ul>
+				<?php
 			}
 		?>
-		</ul>
 		<?php
-			if(isset($lyrics))
+			if(isset($body[SoapExperiment::LYRIC]))
 			{
-				echo "<h2>Lyrics | ".$lyrics['artist']." - ".$lyrics['song']."</h2>";
-				echo $lyrics['lyric'];
+				echo "<h2>Lyrics | ".$body[SoapExperiment::ARTIST]." - ".$body[SoapExperiment::SONG]."</h2>";
+				echo $body[SoapExperiment::LYRIC];
 			}
 		?>
 		<?php 
-			if(isset($error))
+			if(isset($body[SoapExperiment::ERROR]))
 			{
-				echo "<p>".$error."</p>";
+				echo "<p>".$body[SoapExperiment::ERROR]."</p>";
 			}
 		?>
 	</div>
